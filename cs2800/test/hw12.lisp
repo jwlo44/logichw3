f84d4a1ba6edc755ceab50b30dda668c75869473
wightdylan <dylangwight@gmail.com>
2017-11-26 20:09:00 -0500

    starting

--- cs2800/test/hw12.lisp
+++ cs2800/test/hw12.lisp
@@ -1,3 +1,61 @@
+; **************** BEGIN INITIALIZATION FOR ACL2s B MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+
+#|
+Pete Manolios
+Fri Jan 27 09:39:00 EST 2012
+----------------------------
+
+Made changes for spring 2012.
+
+
+Pete Manolios
+Thu Jan 27 18:53:33 EST 2011
+----------------------------
+
+The Beginner level is the next level after Bare Bones level.
+
+|#
+
+; Put CCG book first in order, since it seems this results in faster loading of this mode.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "ccg/ccg" :uncertified-okp nil :dir :acl2s-modes :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+
+;Common base theory for all modes.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "base-theory" :dir :acl2s-modes)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "custom" :dir :acl2s-modes :uncertified-okp nil :ttags :all)
+
+;Settings common to all ACL2s modes
+(acl2s-common-settings)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading trace-star and evalable-ld-printing books.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "trace-star" :uncertified-okp nil :dir :acl2s-modes :ttags ((:acl2s-interaction)) :load-compiled-file nil)
+(include-book "hacking/evalable-ld-printing" :uncertified-okp nil :dir :system :ttags ((:evalable-ld-printing)) :load-compiled-file nil)
+
+;theory for beginner mode
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s beginner theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "beginner-theory" :dir :acl2s-modes :ttags :all)
+
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s Beginner mode.") (value :invisible))
+;Settings specific to ACL2s Beginner mode.
+(acl2s-beginner-settings)
+
+; why why why why 
+(acl2::xdoc acl2s::defunc) ; almost 3 seconds
+
+(cw "~@0Beginner mode loaded.~%~@1"
+    #+acl2s-startup "${NoMoReSnIp}$~%" #-acl2s-startup ""
+    #+acl2s-startup "${SnIpMeHeRe}$~%" #-acl2s-startup "")
+
+
+(acl2::in-package "ACL2S B")
+
+; ***************** END INITIALIZATION FOR ACL2s B MODE ******************* ;
+;$ACL2s-SMode$;Beginner
 #|
 
 CS 2800 Homework 12 - Fall 2017
@@ -28,7 +86,7 @@ If your group does not already exist:
    it will cost you points, so please read carefully.
 
 
-Names of ALL group members: FirstName1 LastName1, FirstName2 LastName2, ...
+Names of ALL group members: Julia Wlochowski, Dylan Wight
 
 Note: There will be a 10 pt penalty if your names do not follow 
 this format.
@@ -194,11 +252,9 @@ If fn is NOT admissible,
       b) If the issue is with conditions 4-6, then give an input that will illustrate
          the violation.
    2) Give the (invalid) induction scheme that fn gives rise to.
-|#
-
-#|
+   
 A1.
-
+|#
 (defunc f1 (x y z)
   :input-contract (and (natp x) (natp y) (listp z))
   :output-contract (natp (f1 x y z))
@@ -206,17 +262,25 @@ A1.
         ((<= (len z) y) (* (len z) y))
         ((> x (len z)) (+ x (f1 x y (rest z))))
         (t (f1 (- x 10) y z))))
-    
-##..............
+#|
+
+admissible,
+(defunc m1 (x y z)
+  :input-contract (and (natp x) (natp y) (listp z))
+  :output-contract (natp (m1 x y z))
+  (+ x (len z)))
+
+phi_1: (and (> (m1 x y z) (m1 x y (rest z))) (> (m1 x y z) (m1 (- x 10) y z))) ... TODO make sure this is right
+(not (and (natp x) (natp y) (listp z))) => phi_1
+(and (natp x) (natp y) (listp z) (< x 10)) => phi_1
+(and (natp x) (natp y) (listp z) (not (< x 10)) (<= (len z) y)) => phi_1
+(and (natp x) (natp y) (listp z) (not (< x 10)) (not (<= (len z) y)) 
+     (> x (len z)) (phi [((z (rest z)))])) => phi_1
+(and (natp x) (natp y) (listp z) (not (< x 10)) (not (<= (len z) y)) 
+     (not (> x (len z))) (phi [((x (- x 10)))])) => phi_1
 
 
 |#
-
-#|
-;; Remember, induction schemes rely on converting a function to an equivalent
-;; cond statement (at least if you want to use the design pattern for creating an IS
-;; given in the notes)
-
 (defunc f2 (x y)
   :input-contract (and (integerp x)(integerp y))
   :output-contract (integerp (f2 x y))
@@ -227,8 +291,36 @@ A1.
         (f2 (+ x 1) y)
         (f2 x (- y 1)))
       x)))
+#|
+;; Remember, induction schemes rely on converting a function to an equivalent
+;; cond statement (at least if you want to use the design pattern for creating an IS
+;; given in the notes)
 
-##..............
+
+
+  (defunc f2* (x y)
+        :input-contract (and (integerp x)(integerp y))
+        :output-contract (integerp (f2* x y))
+        (cond ((natp x) y)
+              ((not (natp y)) x)
+              ((<= (unary-- x) y) (f2 (+ x 1) y))
+              (t (f2 x (- y 1)))))
+      
+              
+admissible,
+(defunc m2 (x y)
+  :input-contract (and (integerp x)(integerp y))
+  :output-contract (natp (m2 x y))
+  (if (<= (unary-- x) y) 
+      (unary-- x) 
+      y))
+  
+(not (and (integerp x)(integerp y))) => phi_2
+(and (integerp x)(integerp y)(natp x)) => phi_2
+(and (integerp x)(integerp y)(not (natp x)) (not (natp y))) => phi_2
+(and (integerp x)(integerp y)(not (natp x)) (natp y) (<= (unary-- x) y) (phi[((x (+ x 1)))])) => phi_2
+(and (integerp x)(integerp y)(not (natp x)) (natp y) (not (<= (unary-- x) y)) (phi[((y (- y 1)))])) => phi_2
+
 
 |#
 
@@ -236,7 +328,10 @@ A1.
 (defunc evenp (i)
   :input-contract (integerp i)
   :output-contract (booleanp (evenp i))
-  (integerp (/ i 2)))
+  (integerp (/ i 2)))#|ACL2s-ToDo-Line|#
+
+
+
 
 #|
 A3.
@@ -246,8 +341,14 @@ A3.
   (cond ((< (len x) 5) (f3 (cons 10 (app x x))))
         ((evenp (len x))  (f3 (cons (* 2 (first x)) x)))
         (t (+ (len x) (first x)))))
-
-##..............
+ 
+admissable,
+(defunc m3 (x)
+  :input-contract (lorp x)
+  :output-contract (natp (m3 x))
+  (if less than 5, 5 - len(x)
+  if even 1
+  else 0
 
 
 |#
